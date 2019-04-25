b80547d6988688b429f7f419faaebd238afdba2d
Adrien Pichard <adrien.pichard@epitech.eu>
2019-04-25 15:45:04 +0200

    coding style v3

--- asm/incl/op.h
+++ asm/incl/op.h
@@ -8,28 +8,28 @@
 #ifndef _OP_H_
 # define _OP_H_
 
-# define MEM_SIZE                (6*1024)
-# define IDX_MOD                 512
-# define MAX_ARGS_NUMBER         4
+# define MEM_SIZE (6*1024)
+# define IDX_MOD 512
+# define MAX_ARGS_NUMBER 4
 
-# define COMMENT_CHAR            '#'
-# define LABEL_CHAR              ':'
-# define DIRECT_CHAR             '%'
-# define SEPARATOR_CHAR          ','
+# define COMMENT_CHAR '#'
+# define LABEL_CHAR ':'
+# define DIRECT_CHAR '%'
+# define SEPARATOR_CHAR ','
 
-# define LABEL_CHARS             "abcdefghijklmnopqrstuvwxyz_0123456789"
+# define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
 
-# define NAME_CMD_STRING         ".name"
-# define COMMENT_CMD_STRING      ".comment"
+# define NAME_CMD_STRING ".name"
+# define COMMENT_CMD_STRING ".comment"
 
-# define REG_NUMBER      16
+# define REG_NUMBER 16
 
-typedef char    args_type_t;
+typedef char args_type_t;
 
-# define T_REG           1
-# define T_DIR           2
-# define T_IND           4
-# define T_LAB           8
+# define T_REG 1
+# define T_DIR 2
+# define T_IND 4
+# define T_LAB 8
 
 typedef struct  op_s
 {
@@ -41,16 +41,16 @@ typedef struct  op_s
    char         *comment;
 } op_t;
 
-# define IND_SIZE        2
-# define DIR_SIZE        4
-# define REG_SIZE        DIR_SIZE
+# define IND_SIZE 2
+# define DIR_SIZE 4
+# define REG_SIZE DIR_SIZE
 
 //extern  op_t    op_tab[];
 
-# define PROG_NAME_LENGTH        128
-# define COMMENT_LENGTH          2048
+# define PROG_NAME_LENGTH 128
+# define COMMENT_LENGTH 2048
 
-# define COREWAR_EXEC_MAGIC      0xea83f3
+# define COREWAR_EXEC_MAGIC 0xea83f3
 
 typedef struct header_s
 {
@@ -60,8 +60,8 @@ typedef struct header_s
    char comment[COMMENT_LENGTH + 1];
 } header_t;
 
-# define CYCLE_TO_DIE    1536
-# define CYCLE_DELTA     5
-# define NBR_LIVE        40
+# define CYCLE_TO_DIE 1536
+# define CYCLE_DELTA 5
+# define NBR_LIVE 40
 
 #endif
