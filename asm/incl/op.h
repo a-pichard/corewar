1e82c5125f065ed02e3644e17eb0b2926e716ef1
Adrien Pichard <adrien.pichard@epitech.eu>
2019-04-25 15:46:41 +0200

    coding style v4

--- asm/incl/op.h
+++ asm/incl/op.h
@@ -6,39 +6,39 @@
 */
 
 #ifndef _OP_H_
-# define _OP_H_
+#define _OP_H_
 
-# define MEM_SIZE (6*1024)
-# define IDX_MOD 512
-# define MAX_ARGS_NUMBER 4
+#define MEM_SIZE (6*1024)
+#define IDX_MOD 512
+#define MAX_ARGS_NUMBER 4
 
-# define COMMENT_CHAR '#'
-# define LABEL_CHAR ':'
-# define DIRECT_CHAR '%'
-# define SEPARATOR_CHAR ','
+#define COMMENT_CHAR '#'
+#define LABEL_CHAR ':'
+#define DIRECT_CHAR '%'
+#define SEPARATOR_CHAR ','
 
-# define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
+#define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
 
-# define NAME_CMD_STRING ".name"
-# define COMMENT_CMD_STRING ".comment"
+#define NAME_CMD_STRING ".name"
+#define COMMENT_CMD_STRING ".comment"
 
-# define REG_NUMBER 16
+#define REG_NUMBER 16
 
 typedef char args_type_t;
 
-# define T_REG 1
-# define T_DIR 2
-# define T_IND 4
-# define T_LAB 8
+#define T_REG 1
+#define T_DIR 2
+#define T_IND 4
+#define T_LAB 8
 
 typedef struct  op_s
 {
-   char         *mnemonique;
-   char         nbr_args;
-   args_type_t  type[MAX_ARGS_NUMBER];
-   char         code;
-   int          nbr_cycles;
-   char         *comment;
+   char *mnemonique;
+   char nbr_args;
+   args_type_t type[MAX_ARGS_NUMBER];
+   char code;
+   int nbr_cycles;
+   char *comment;
 } op_t;
 
 # define IND_SIZE 2
@@ -54,9 +54,9 @@ typedef struct  op_s
 
 typedef struct header_s
 {
-   int  magic;
+   int magic;
    char prog_name[PROG_NAME_LENGTH + 1];
-   int  prog_size;
+   int prog_size;
    char comment[COMMENT_LENGTH + 1];
 } header_t;
 
