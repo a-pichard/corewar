004111b6677687205c0edba69d20db8f61bac059
Adrien Pichard <adrien.pichard@epitech.eu>
2019-04-26 16:06:57 +0200

    ADD: lit to big endian

---
+++ asm/src/utils/lit_to_big_endian.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_corewar_2018
+** File description:
+** lit_to_big_endian
+*/
+
+int lit_to_big_endian(int x)
+{
+	return (((x>>24) & 0x000000ff) | ((x>>8) & 0x0000ff00) | ((x<<8) & 0x00ff0000) | ((x<<24) & 0xff000000));
+}
