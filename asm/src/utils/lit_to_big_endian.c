e7cba985b3dbc485a333fc65507a01996c64b7ea
thomas roux <thomasroux021@gmail.com>
2019-05-03 15:05:32 +0200

    no leak are now possible and handling lit or big endian

--- asm/src/utils/lit_to_big_endian.c
+++ asm/src/utils/lit_to_big_endian.c
@@ -7,6 +7,11 @@
 
 int lit_to_big_endian(int x)
 {
+    unsigned int n = 1;
+    char *mem = (char *) &n;
+
+    if (mem[0] != 1)
+        return (x);
     return (((x >> 24) & 0x000000ff) | ((x >> 8) & 0x0000ff00) |
             ((x << 8) & 0x00ff0000) | ((x << 24) & 0xff000000));
 }
