1a78e855d1d28c0ff1e0c0f9b0ccc5355a7e6145
Besset Rémi <bremi@localhost.localdomain>
2019-05-17 14:41:13 +0200

    add: lld instrcution

--- corewar/src/mnemonic/ld.c
+++ corewar/src/mnemonic/ld.c
@@ -14,16 +14,16 @@ void ld(corewar_t *cor, vec_t *proc, int n)
 {
     int pc = ((process_t *)proc->content[n])->pc;
     op_t op_tab[] = {OP_TAB};
-    int *arg = get_args(cor->memory, pc, 3);
-    char *type = conv_i_str(dec_to_bin(arg[0]));
+    int *args = get_args(cor->memory, pc, 3);
+    char *type = conv_i_str(dec_to_bin(args[0]));
     int i_chmp = ((process_t *) proc->content[n])->chmp;
 
-    if (type[2] - 48 == 0 && REG_VALID(arg[2])) {
-        ((process_t *)proc->content[n])->reg[arg[2]] = pc + arg[1] % IDX_MOD;
+    if (type[2] - 48 == 0 && REG_VALID(args[2])) {
+        ((process_t *)proc->content[n])->reg[args[2]] = pc + args[1] % IDX_MOD;
         ((process_t *)proc->content[n])->pc = (pc + 4) % MEM_SIZE;
     } else
         ((process_t *)proc->content[n])->pc = (pc + 1) % MEM_SIZE;
     ((process_t *)proc->content[n])->sleep = op_tab[1].nbr_cycles;
-    cor->prgs[i_chmp]->carry = (arg[2] == 0);
+    cor->prgs[i_chmp]->carry = (args[2] == 0);
     free(type);
 }
--- corewar/src/mnemonic/lld.c
+++ corewar/src/mnemonic/lld.c
@@ -11,5 +11,18 @@
 
 void lld(corewar_t *cor, vec_t *proc, int n)
 {
+    int pc = ((process_t *)proc->content[n])->pc;
+    op_t op_tab[] = {OP_TAB};
+    int *args = get_args(cor->memory, pc, 3);
+    char *type = conv_i_str(dec_to_bin(args[0]));
+    int i_chmp = ((process_t *) proc->content[n])->chmp;
 
+    if (type[2] - 48 == 0 && REG_VALID(args[2])) {
+        ((process_t *)proc->content[n])->reg[args[2]] = pc + args[1];
+        ((process_t *)proc->content[n])->pc = (pc + 4) % MEM_SIZE;
+    } else
+        ((process_t *)proc->content[n])->pc = (pc + 1) % MEM_SIZE;
+    ((process_t *)proc->content[n])->sleep = op_tab[1].nbr_cycles;
+    cor->prgs[i_chmp]->carry = (args[2] == 0);
+    free(type);
 }
